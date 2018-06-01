8994f1684bf5458848de075b2a475524b4e37f6e
liuhuan <510833604@qq.com>
2018-06-01 23:16:48 +0800

    // update version and setup file

--- setup.bat
+++ setup.bat
@@ -5,10 +5,11 @@ call git clone https://github.com/neuecc/UniRx.git unirx
 echo "Zenject"
 call git clone https://github.com/modesttree/Zenject.git zenject
 echo "Setup symlinks"
-call mklink /D assets-folder\Zenject ..\zenject\UnityProject\Assets\Zenject
+call mklink /D assets-folder\Zenject ..\zenject\UnityProject\Assets\Plugins\Zenject
 call mklink /D assets-folder\Plugins\UniRx ..\..\unirx\Assets\Plugins\UniRx
 call mklink /D llapi-client\Assets ..\assets-folder
 call mklink /D llapi-server\Assets ..\assets-folder
-call mklink /D llapi-client\ProjectSettings ..\project-settings
-call mklink /D llapi-server\ProjectSettings ..\project-settings
+call mklink /D llapi-client\ProjectSettings ..\..\project-settings
+call mklink /D llapi-server\ProjectSettings ..\..\project-settings
 echo "Done"
+pause
